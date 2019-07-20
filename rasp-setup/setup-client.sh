1a509e3ce53fe2c56e68ef2f049c180f5a74fd06
Alexander Frank <ap.frank@tum.de>
2019-07-20 21:19:18 +0200

    add separate folder to store backups in case something goes wrong

--- rasp-setup/setup-client.sh
+++ rasp-setup/setup-client.sh
@@ -3,10 +3,15 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$THIS_DIR"
 
+mkdir backups
+
 sudo apt update
 sudo apt install arp-scan
 
-sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.orig
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf backups/wpa_supplicant.conf.orig
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/
 
-
+# this makes sure that only wlan0 is configured by dhcp, the rest is ignore
+# https://serverfault.com/a/880575
+sudo cp /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant backups/10-wpa_supplicant.orig
+sudo sed -i 's,if \[ "\$ifwireless" = "1" \] \&\& \\,if \[ "\$ifwireless" = "1" \] \&\& \[ "\$interface" = "wlan0" \] \&\& \\,g' /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
--- rasp-setup/setup-server.sh
+++ rasp-setup/setup-server.sh
@@ -3,6 +3,8 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$THIS_DIR"
 
+mkdir -p backups
+
 # install tools if neccessary
 sudo apt update
 sudo apt install dnsmasq hostapd arp-scan
@@ -11,12 +13,12 @@ sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
 
 # set up dhcp
-sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+sudo mv /etc/dhcpcd.conf backups/dhcpcd.conf.orig
 sudo cp dhcpcd.conf /etc/
 
 sudo service dhcpcd restart
 
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo mv /etc/dnsmasq.conf backups/dnsmasq.conf.orig
 sudo cp dnsmasq.conf /etc/
 
 sudo service dnsmasq restart
@@ -33,6 +35,6 @@ sudo cp sysctl.conf /etc
 sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
-sudo cp /etc/rc.local /etc/rc.local.orig
+sudo cp /etc/rc.local backups/rc.local.orig
 sudo sed -i "s,exit 0,iptables-restore < /etc/iptables.ipv4.nat\nexit 0,g" /etc/rc.local
 
